1f3a8a93f1987bc04c25a91d5b736bac4634844f
huzhuang-x <704955775@qq.com>
2023-01-05 17:08:49 +0800

    【eureka】服务端搭建

---
+++ eureka-server/src/test/java/com/hz/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hz.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
